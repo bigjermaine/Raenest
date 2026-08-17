2a985814887256721aa3ebf662f0727d2bd97133
BigJermaine <113020989+bigjermaine@users.noreply.github.com>
2026-08-17 23:19:23 +0100

    Initial Commit

---
+++ Raenest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Raenest
+//
+//  Created by jermaine daniel on 17/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Raenest/RaenestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaenestApp.swift
+//  Raenest
+//
+//  Created by jermaine daniel on 17/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct RaenestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
